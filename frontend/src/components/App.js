fb1867f4614a7f63228390968c4a07aa54653f45
VitMach <machihin_vitalii@mail.ru>
2023-05-19 00:14:51 +0300

    Объединение frontend и backend\

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -201,7 +201,7 @@ function App() {
 
   return (
     <div className="page">
-      <CurrentUserContext.Provider value={currentUser}>
+      <CurrentUserContext.Provider value={{ currentUser }}>
         <Header userEmail={userEmail} onSignOut={handleSignOut} />
 
         <Routes>
